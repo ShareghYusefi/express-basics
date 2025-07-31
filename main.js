c8dc3353b7c11903fe5ac3f9add40ff3bbf4abbc
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-07-30 19:21:09 -0600

    custom middleware

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const express = require("express");
 
 const app = express();
 
+// A middleware is a function that has access to the request and response object
+// you can think of a middleware as a layer that sits between the request and response.
+function customMiddleware(req, res, next) {
+  console.log("Middleware function called!");
+  // next function is called to move onto the next middleware function
+  next();
+}
+
+// use the middleware function when a request comes in from the web.
+app.use(customMiddleware);
+
 // localhost:3000/
 app.get("/", (req, res) => {
   res.send("Hello World");
